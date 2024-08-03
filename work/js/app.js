c71e9d200293c6bbb3fd23f1b42b01c92c2f5f3d
ChihiroTanaka <tokyo.9st@gmail.com>
2024-08-03 11:43:01 +0900

    commit

---
+++ work/js/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',function() {
+  fetch('data/product.json')
+  .then(response => response.json())
+  .then(products => {
+    const productTable = document.getElementById('productTable');
+    for (let i = 0; i < products.length; i++){
+      const product = products[i];
+      const tableRow =  Object.assign(document.createElement('tr'),
+      {innerHTML:
+        `<td>${product.id}</td>
+        <td>${product.name}</td>
+        <td>${product.price}</td>
+        <td>${product.description}</td>`
+      })
+        productTable.appendChild(tableRow);
+      }
+    })
+});
